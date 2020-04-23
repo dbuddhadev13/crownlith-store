e659fa6fe03c75abd888339cc38c6c737dd9bfde
Dhyey Buddhadev <dbuddhadev@gmail.com>
2020-04-23 21:02:24 +0530

    Initial Shop Commit with components and Pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './Pages/Home/Home';
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
