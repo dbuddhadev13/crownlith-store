9ab8f1254c21207bdb8266e4907860b10092ff5a
Dhyey Buddhadev <dbuddhadev@gmail.com>
2020-04-24 00:01:50 +0530

    Adding Header Component and modify CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import { Route, Switch } from 'react-router-dom';
+import Header from './Components/Header/header.component';
 
 import HomePage from './Pages/Home/Home';
 import ShopPage from './Pages/Shop/shop.component';
@@ -9,6 +10,7 @@ import ShopPage from './Pages/Shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
