169f6aa38d1f58345d6d2b7a0f21ea90955eca93
Dhyey Buddhadev <dbuddhadev@gmail.com>
2020-04-30 14:00:26 +0530

    Added Backend Server using Node and ExpressJS. Moved frontend code to client folder

--- client/src/Components/Stripe-button/stripe-button.component.jsx
+++ client/src/Components/Stripe-button/stripe-button.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from 'axios'
 const StripeButton = ({ price }) => {
     const priceForStripe = price * 100
     const publishableKey = "pk_test_j4heub1l4BZjoLFWatTA1aCR00cv2Ive4p"
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        }).then(response => {
+            alert("Payment Successful")
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert("There was an issue with your payment. Please use the provided test credit card")
+        })
     }
 
     return (
